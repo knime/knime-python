63543fb93ad19b47adab4879eafb7a42b7f5f31b
Patrick Winter <patrick.winter@uni-konstanz.de>
2014-11-20 12:30:15 +0000

    Python: Added version checks to kernel tester

--- org.knime.python/py/PythonKernelTester.py
+++ org.knime.python/py/PythonKernelTester.py
@@ -3,7 +3,6 @@
 import sys
 
 _message = ''
-_error = False
 
 
 # check libs, output info and exit with 1 if an error occurred
@@ -12,8 +11,6 @@ def main():
     print('Python version: ' + str(pythonVersion[0]) + '.' + str(pythonVersion[1]) + '.' + str(pythonVersion[2]))
     check_required_libs()
     print(_message)
-    if _error:
-        sys.exit(1)
 
 
 # check for all libs that are required by the python kernel
@@ -38,18 +35,18 @@ def check_required_libs():
     check_lib('numpy')
     check_lib('pandas', ['DataFrame'])
     check_lib('google.protobuf')
+    # check lib versions
+    check_version_python()
+    check_version_pandas()
 
 
 def check_lib(lib, cls=[]):
-    global _message, _error
     if not lib_available(lib):
-        _message += 'Library ' + lib + ' is missing\n'
-        _error = True
+        add_to_message('Library ' + lib + ' is missing')
     else:
         for cl in cls:
             if not class_available(lib, cl):
-                _message += 'Class ' + cl + ' in library ' + lib + ' is missing\n'
-                _error = True
+                add_to_message('Class ' + cl + ' in library ' + lib + ' is missing')
 
 
 def class_available(lib, cls):
@@ -62,6 +59,49 @@ def lib_available(lib):
     local_env = {}
     exec('try:\n\timport ' + lib + '\n\tsuccess = True\nexcept:\n\tsuccess = False', {}, local_env)
     return local_env['success']
+
+
+def check_version_python():
+    min_version = '2.5.0'.split('.')
+    version = sys.version_info
+    smaller = False
+    bigger = False
+    for i in range(len(min_version)):
+        if int(version[i]) > int(min_version[i]):
+            bigger = True
+            break
+        if not bigger and int(version[i]) < int(min_version[i]):
+            smaller = True
+            break
+    if smaller:
+        add_to_message('Installed python version is ' + str(version[0]) + '.' + str(version[1]) + '.' + str(version[2]) + ', required minimum is ' + '.'.join(min_version))
+
+
+def check_version_pandas():
+    min_version = '0.13.0'.split('.')
+    try:
+        import pandas
+        version = pandas.__version__.split('.')
+        if len(version) is not len(min_version):
+            raise Exception()
+        smaller = False
+        bigger = False
+        for i in range(len(min_version)):
+            if int(version[i]) > int(min_version[i]):
+                bigger = True
+                break
+            if not bigger and int(version[i]) < int(min_version[i]):
+                smaller = True
+                break
+        if smaller:
+            add_to_message('Installed pandas version is ' + '.'.join(version) + ', required minimum is ' + '.'.join(min_version))
+    except:
+        add_to_message('Could not detect pandas version')
+
+
+def add_to_message(line):
+    global _message
+    _message += line + '\n'
 
 
 main()
