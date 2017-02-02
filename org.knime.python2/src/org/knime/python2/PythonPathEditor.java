439ad111dad656ba9ea67e0e134e1234b602c4b3
Patrick Winter <patrick.winter@uni-konstanz.de>
2017-02-02 13:34:18 +0100

    Fixed layout of preference page.

--- org.knime.python2/src/org/knime/python2/PythonPathEditor.java
+++ org.knime.python2/src/org/knime/python2/PythonPathEditor.java
@@ -2,12 +2,10 @@ package org.knime.python2;
 
 import org.eclipse.jface.preference.FileFieldEditor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
@@ -16,26 +14,27 @@ public class PythonPathEditor extends Composite {
 	private FileFieldEditor m_pathEditor;
 	private Label m_info;
 	private Label m_error;
-	private ScrolledComposite m_sc;
-	Composite m_container;
 
 	public PythonPathEditor(final String label, final Composite parent) {
 		super(parent, SWT.NONE);
-		m_sc = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL);
-		m_container = new Composite(m_sc, SWT.NONE);
-		m_container.setLayout(new GridLayout());
-		m_pathEditor = new FileFieldEditor(Activator.PLUGIN_ID, label, m_container);
-		m_pathEditor.setStringValue(getPythonPath());
 		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.grabExcessVerticalSpace = true;
+		gridData.horizontalAlignment = SWT.FILL;
+		gridData.verticalAlignment = SWT.FILL;
+		setLayoutData(gridData);
+		m_pathEditor = new FileFieldEditor(Activator.PLUGIN_ID, label, this);
+		m_pathEditor.setStringValue(getPythonPath());
+		gridData = new GridData();
 		gridData.horizontalSpan = 3;
 		gridData = new GridData();
 		gridData.horizontalSpan = 3;
 		gridData.verticalIndent = 20;
-		m_info = new Label(m_container, SWT.NONE);
+		m_info = new Label(this, SWT.NONE);
 		m_info.setLayoutData(gridData);
 		gridData = new GridData();
 		gridData.horizontalSpan = 3;
-		m_error = new Label(m_container, SWT.NONE);
+		m_error = new Label(this, SWT.NONE);
 		m_error.setLayoutData(gridData);
 		final Color red = new Color(parent.getDisplay(), 255, 0, 0);
 		m_error.setForeground(red);
@@ -45,9 +44,6 @@ public class PythonPathEditor extends Composite {
 				red.dispose();
 			}
 		});
-		m_sc.setContent(m_container);
-		m_sc.setExpandHorizontal(true);
-		m_sc.setExpandVertical(true);
 	}
 	
 	public void setPythonPath(final String pythonPath) {
@@ -72,8 +68,7 @@ public class PythonPathEditor extends Composite {
 	 * Refreshes the pages layout and size.
 	 */
 	private void refreshSizes() {
-		m_container.layout();
-		m_sc.setMinSize(m_container.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		layout();
 	}
 
 }
