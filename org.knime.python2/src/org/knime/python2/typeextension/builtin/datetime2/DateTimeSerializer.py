2beaaf25cfecbd26553ffff9fe79d25196157521
clemens-vonschwerin-knime <clemens.vonschwerin@knime.com>
2017-05-12 11:59:38 +0200

    AP-7241 Improved Python Integration
    Renamed nodes and moved to labs

--- org.knime.python2/src/org/knime/python2/typeextension/builtin/datetime2/DateTimeSerializer.py
+++ org.knime.python2/src/org/knime/python2/typeextension/builtin/datetime2/DateTimeSerializer.py
@@ -3,7 +3,7 @@ _format = '%Y-%m-%d %H:%M:%S.%f'
 
 def serialize(object_value):
     datestr = object_value.strftime(_format)[:-3]
-    #Note: The datetime API only handles dates strating from year 1, no need to worry about negative years (clemens.vonschwerin@knime.com)
+    #Note: The datetime API only handles dates starting from year 1, no need to worry about negative years (clemens.vonschwerin@knime.com)
     if object_value.year < 10:
         datestr = '000' + datestr
     elif object_value.year < 100:
