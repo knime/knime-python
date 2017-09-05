f3c9ac97a997632e88f5cb9381496766b53842ed
Clemens von Schwerin <clemens.vonschwerin@knime.com>
2017-09-05 16:11:51 +0200

    AP-7927: Future warnings are suppressed.
    (AP-7927 Suppress pandas tslib warning in old python integration)

--- org.knime.python/py/PythonKernel.py
+++ org.knime.python/py/PythonKernel.py
@@ -28,6 +28,10 @@ import sqlOutput_pb2
 import variables_pb2
 from DBUtil import *
 
+# suppress FutureWarnings
+import warnings
+warnings.filterwarnings(action='ignore', category=FutureWarning)
+
 # check if we are running python 2 or python 3
 _python3 = sys.version_info >= (3, 0)
 
