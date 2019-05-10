05265145e9e8b4013a87f31f5166331dd4ad7ab8
Sebastian Gerau <sebastian.gerau@knime.com>
2019-05-10 11:19:45 +0200

    Up-to-date Anaconda3 install script.

--- org.knime.python2.envconfigs/buildscripts/install-anaconda.sh
+++ org.knime.python2.envconfigs/buildscripts/install-anaconda.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-cd python
+set -x
+cd ~/python
 if [[ ! -d ~/python/anaconda ]]; then
-	if [[ "$(uname -s)" == "Darwin" ]]; then
-		INST_FILE=Anaconda3-4.4.0-MacOSX-x86_64.sh
-	else
-		INST_FILE=Anaconda3-4.4.0-Linux-x86_64.sh
-	fi
+        if [[ "$(uname -s)" == "Darwin" ]]; then
+                INST_FILE=Anaconda3-4.4.0-MacOSX-x86_64.sh
+        else
+                INST_FILE=Anaconda3-4.4.0-Linux-x86_64.sh
+        fi
 
-	wget -qN https://repo.continuum.io/archive/$INST_FILE
-	bash $INST_FILE -b -p ~/python/anaconda
-	rm $INST_FILE
+        wget -qN https://repo.continuum.io/archive/$INST_FILE
+        bash $INST_FILE -b -p ~/python/anaconda
+        rm $INST_FILE
 fi
 
 export PATH=~/python/anaconda/bin:$PATH
 
-conda env remove -n py35_knime -q -y
-conda env remove -n py27_knime -q -y
+conda env remove -n py36_knime -q -y || true
+conda env remove -n py27_knime -q -y || true
 
-conda env create -f py35_knime.yml -q
-conda env create -f py27_knime.yml -q
+conda env remove -n py3_knime -q -y || true
+conda env remove -n py2_knime -q -y || true
 
-if [[ "$(uname -s)" == "Linux" ]]; then
-	source activate py35_knime
-	pip install https://cntk.ai/PythonWheel/CPU-Only/cntk-2.2-cp35-cp35m-linux_x86_64.whl
-fi
-
+conda env create -f py3_knime.yml -q
+conda env create -f py2_knime.yml -q
