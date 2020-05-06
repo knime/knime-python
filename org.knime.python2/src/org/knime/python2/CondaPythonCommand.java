944ecdb7e6416e7005c18f21aa7b28d2165c4b94
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2020-05-06 19:18:32 +0200

    AP-13723: Prepend library paths to PATH on Windows
    AP-13723 (Stop invoking conda activate)

--- org.knime.python2/src/org/knime/python2/CondaPythonCommand.java
+++ org.knime.python2/src/org/knime/python2/CondaPythonCommand.java
@@ -48,21 +48,31 @@
  */
 package org.knime.python2;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang3.SystemUtils;
 
 /**
  * Conda-specific implementation of {@link PythonCommand}. Allows to build Python processes for a given Conda
- * installation and environment name.
+ * installation and environment name. Takes care of resolving PATH-related issues on Windows.
  *
  * @author Marcel Wiedenmann, KNIME GmbH, Konstanz, Germany
  */
 public final class CondaPythonCommand extends AbstractPythonCommand {
 
     private static final String CONDA_ENVIRONMENTS_DIRECTORY_NAME = "envs";
+
+    private static final String PATH_ENVIRONMENT_VARIABLE_NAME = "PATH";
+
+    /**
+     * Note: path as in the PATH environment variable, not the path to the Python executable.
+     */
+    private final String m_pathPrefix;
 
     /**
      * Constructs a {@link PythonCommand} that describes a Python process of the given Python version that is run in the
@@ -76,6 +86,23 @@ public final class CondaPythonCommand extends AbstractPythonCommand {
     public CondaPythonCommand(final PythonVersion pythonVersion, final String condaInstallationDirectoryPath,
         final String environmentName) {
         super(pythonVersion, Arrays.asList(createExecutableString(condaInstallationDirectoryPath, environmentName)));
+        // On Windows, we need to prepend a number of library paths to the PATH environment variable to resolve issues
+        // that may occur when Python modules are searching for DLLs.
+        if (SystemUtils.IS_OS_WINDOWS) {
+            final List<String> pathPrefixes = new ArrayList<>();
+            final String environmentDirectory =
+                createEnvironmentDirectoryString(condaInstallationDirectoryPath, environmentName);
+            addToPrefixes(pathPrefixes, environmentDirectory);
+            addToPrefixes(pathPrefixes, environmentDirectory, "Library", "mingw-w64", "bin");
+            addToPrefixes(pathPrefixes, environmentDirectory, "Library", "usr", "bin");
+            addToPrefixes(pathPrefixes, environmentDirectory, "Library", "bin");
+            addToPrefixes(pathPrefixes, environmentDirectory, "Scripts");
+            addToPrefixes(pathPrefixes, environmentDirectory, "bin");
+            addToPrefixes(pathPrefixes, condaInstallationDirectoryPath, "condabin");
+            m_pathPrefix = String.join(File.pathSeparator, pathPrefixes);
+        } else {
+            m_pathPrefix = null;
+        }
     }
 
     /**
@@ -83,16 +110,38 @@ public final class CondaPythonCommand extends AbstractPythonCommand {
      */
     private static String createExecutableString(final String condaInstallationDirectoryPath,
         final String environmentName) {
+        final String environmentDirectory =
+            createEnvironmentDirectoryString(condaInstallationDirectoryPath, environmentName);
         final Path executablePath;
         if (SystemUtils.IS_OS_LINUX || SystemUtils.IS_OS_MAC) {
-            executablePath = Paths.get(condaInstallationDirectoryPath, CONDA_ENVIRONMENTS_DIRECTORY_NAME,
-                environmentName, "bin", "python");
+            executablePath = Paths.get(environmentDirectory, "bin", "python");
         } else if (SystemUtils.IS_OS_WINDOWS) {
-            executablePath = Paths.get(condaInstallationDirectoryPath, CONDA_ENVIRONMENTS_DIRECTORY_NAME,
-                environmentName, "python.exe");
+            executablePath = Paths.get(environmentDirectory, "python.exe");
         } else {
             throw Conda.createUnknownOSException();
         }
         return executablePath.toString();
+    }
+
+    /**
+     * Paths are determined as per https://docs.anaconda.com/anaconda/user-guide/tasks/integration/python-path/
+     */
+    private static String createEnvironmentDirectoryString(final String condaInstallationDirectoryPath,
+        final String environmentName) {
+        return Paths.get(condaInstallationDirectoryPath, CONDA_ENVIRONMENTS_DIRECTORY_NAME, environmentName).toString();
+    }
+
+    private static void addToPrefixes(final List<String> prefixes, final String first, final String... more) {
+        prefixes.add(Paths.get(first, more).toString());
+    }
+
+    @Override
+    public ProcessBuilder createProcessBuilder() {
+        final ProcessBuilder pb = super.createProcessBuilder();
+        if (m_pathPrefix != null) {
+            pb.environment().merge(PATH_ENVIRONMENT_VARIABLE_NAME, m_pathPrefix,
+                (oldPath, pathPrefix) -> pathPrefix + File.pathSeparatorChar + oldPath);
+        }
+        return pb;
     }
 }
