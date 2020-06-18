85d351c60e45f711a88c0bed1b5001fd45b541b0
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2020-06-18 17:27:45 +0200

    DEVOPS-418: Remove obsolete build scripts
    DEVOPS-418: Configure Python environments and tests in new build system

--- org.knime.python2.envconfigs/buildscripts/setenv/windows/python-2.bat
+++
@@ -1,4 +0,0 @@
-@SET PATH=C:\tools\Anaconda3\Scripts;%PATH%
-
-@CALL activate py27_knime || ECHO Activating py27_knime failed
-@python %*
--- org.knime.python2.envconfigs/buildscripts/setenv/windows/python-3.bat
+++
@@ -1,4 +0,0 @@
-@SET PATH=C:\tools\Anaconda3\Scripts;%PATH%
-
-@CALL activate py36_knime || ECHO Activating py36_knime failed
-@python %*
