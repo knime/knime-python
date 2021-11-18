6b2bc3660f0464675cf54eb13ec0cb0d99806b7f
Gabriel Einsdorf <gabriel.einsdorf@knime.com>
2021-11-18 16:16:02 +0100

    DEVOPS-870: Add support for running the workflowtests with multiple python versions

---
+++ workflow-tests/setup-Windows.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set python version if required
+if [[ -n $KNIME_WORKFLOWTEST_PYTHON_VERSION ]]; then
+	echo "Setting python version to: ${KNIME_WORKFLOWTEST_PYTHON_VERSION}"
+
+	preferencesPath="${WORKSPACE}/workflow-tests/preferences-Windows.epf"
+	envPath="C:\\\\\\\\Users\\\\\\\\jenkins\\\\\\\\Miniconda3\\\\\\\\envs\\\\\\\\knime_py${KNIME_WORKFLOWTEST_PYTHON_VERSION}"
+	py3EnvPrefKey='python3CondaEnvironmentDirectoryPath='
+
+	sedi "s|${py3EnvPrefKey}.*|${py3EnvPrefKey}${envPath}|g" "${preferencesPath}"
+
+	cat "${preferencesPath}"
+fi
