d2b62d4212e9a501a753f677921c510bda63bdd2
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2022-07-29 14:23:56 +0200

    AP-19109: Add configuration for pytest
    AP-19109 (Enable Python unit tests)

---
+++ conftest.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+# Utils
+
+
+def join(*paths):
+    return os.path.normpath(os.path.join(*paths))
+
+
+def add_path(*rel_path):
+    sys.path.append(join(__file__, "..", *rel_path))
+
+
+def add_src_main_path(plugin_name):
+    add_path(plugin_name, "src", "main", "python")
+
+
+# Ignore the knime_kernel_test.py because it is run from a JUnit test
+
+collect_ignore = [
+    join(
+        "org.knime.python3.scripting.tests",
+        "src",
+        "test",
+        "python",
+        "knime_kernel_test.py",
+    )
+]
+
+# Add paths to the python sources
+
+add_src_main_path("org.knime.python3")
+add_src_main_path("org.knime.python3.nodes")
+add_src_main_path("org.knime.python3.scripting")
+add_src_main_path("org.knime.python3.scripting.tests")
+add_src_main_path("org.knime.python3.views")
+add_src_main_path("org.knime.python3.arrow")
+add_src_main_path("org.knime.python3.arrow.types")
