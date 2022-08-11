6bb01d54a69fbad17d2af71fb691abbcb166d0db
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2022-08-11 18:08:02 +0200

    AP-19297: Enable the black formatting test
    AP-19297 (Enable knime-python py file formatting test)

--- test_black_formatting.py
+++ test_black_formatting.py
@@ -5,10 +5,10 @@ import unittest
 
 
 class BlackFormattingTest(unittest.TestCase):
-    @unittest.skip("black is not installed on the test machine")
     def test_black_formatting(self):
         root_dir = os.path.normpath(os.path.join(__file__, ".."))
-        res = subprocess.run(["black", root_dir, "--check"], capture_output=True)
+        black_path = os.path.join(sys.prefix, "bin", "black")
+        res = subprocess.run([black_path, root_dir, "--check"], capture_output=True)
         if res.returncode != 0:
             print(res.stdout.decode())
             print(res.stderr.decode(), file=sys.stderr)
