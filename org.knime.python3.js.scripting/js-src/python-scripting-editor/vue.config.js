f67378395d0a235fdc43c66fe8c129ac86195cd0
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2022-08-17 09:09:21 +0200

    AP-19330: Allow importing SVGs and add loading animation
    AP-19330 (Python JS-Based Scripting Editor)

--- org.knime.python3.js.scripting/js-src/python-scripting-editor/vue.config.js
+++ org.knime.python3.js.scripting/js-src/python-scripting-editor/vue.config.js
@@ -14,12 +14,43 @@ module.exports = defineConfig({
         ]
     },
     chainWebpack: (config) => {
-    // allow tilde imports
+        // allow tilde imports
         config.resolve.alias.set('~', path.resolve(__dirname));
 
-        config.resolve.alias.set(
-            'webapps-common',
-            path.resolve(__dirname, 'webapps-common')
-        );
+        config.resolve.alias.set('webapps-common', path.resolve(__dirname, 'webapps-common'));
+
+        /* our SVG rule in webapps-common isn't compatible with VueCLI 5 / Webpack 5 */
+        const svgRule = config.module.rule('svg');
+        svgRule.uses.clear();
+        svgRule.delete('type');
+        svgRule.delete('generator');
+
+        svgRule
+            // load svgs as data-url
+            .oneOf('data-url')
+            .resourceQuery(/data/)
+            .use('url-loader')
+            .loader('url-loader')
+            .end()
+            .end()
+
+            // load svgs as file imports
+            .oneOf('file-import')
+            .resourceQuery(/file/)
+            .use('file-loader')
+            .loader('file-loader')
+            .options({
+                name: 'assets/[name].[hash:8].[ext]'
+            })
+            .end()
+            .end()
+
+            // load svgs as inlined elements
+            .oneOf('inline')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .end()
+            .use('vue-svg-loader')
+            .loader('vue-svg-loader');
     }
 });
