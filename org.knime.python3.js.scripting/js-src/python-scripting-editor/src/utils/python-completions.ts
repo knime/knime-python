f153b4a26b254d52ca599c24d61c01a2466dd4ba
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2022-09-22 18:07:10 +0200

    AP-19337: Add completion provider for object inputs
    And improve the way the completion list is computed by only computing it
    once.
    
    AP-19337 (Support dynamic input and output ports with all types of the Labs scripting node)

--- org.knime.python3.js.scripting/js-src/python-scripting-editor/src/utils/python-completions.ts
+++ org.knime.python3.js.scripting/js-src/python-scripting-editor/src/utils/python-completions.ts
@@ -1,51 +1,69 @@
 import * as monaco from 'monaco-editor';
-import { InputPortInfo, InputTableInfo } from './python-scripting-service';
+import { InputPortInfo, InputTableInfo, InputObjectInfo } from './python-scripting-service';
 
 const columnCompletionFor = (
     tableIdx: number,
-    column: string,
-    range: monaco.IRange
-): monaco.languages.CompletionItem[] => ['pyarrow', 'pandas'].map((format) => {
-    const text = `knio.input_table[${tableIdx}].to_${format}()["${column}"]`;
+    tableVarName: string,
+    column: string
+) => ['pyarrow', 'pandas'].map((format) => {
+    const text = `${tableVarName}.to_${format}()["${column}"]`;
     return {
         label: text,
         kind: monaco.languages.CompletionItemKind.Snippet,
         documentation: `Access the column "${column}" of input table ${tableIdx} using ${format}.`,
-        insertText: text,
-        range
+        insertText: text
     };
 });
 
-const tableCompletionFor = (
-    tableIdx: number,
-    table: string[],
-    range: monaco.IRange
-): monaco.languages.CompletionItem[] => {
-    const tableAccess = `knio.input_table[${tableIdx}]`;
-    return [
-        {
-            label: tableAccess,
+const tableCompletionFor = (tableIdx: number, tableInfo: InputTableInfo) => [
+    {
+        label: tableInfo.variableName,
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        documentation: `Access table ${tableIdx}.`,
+        insertText: tableInfo.variableName
+    },
+    ...['pyarrow', 'pandas'].map((format) => {
+        const text = `${tableInfo.variableName}.to_${format}()`;
+        return {
+            label: text,
             kind: monaco.languages.CompletionItemKind.Snippet,
-            documentation: `Access table ${tableIdx}.`,
-            insertText: tableAccess,
-            range
-        },
-        ...['pyarrow', 'pandas'].map((format) => {
-            const text = `${tableAccess}.to_${format}()`;
-            return {
-                label: text,
-                kind: monaco.languages.CompletionItemKind.Snippet,
-                documentation: `Access table ${tableIdx} using ${format}.`,
-                insertText: text,
-                range
-            };
-        }),
-        ...table.flatMap((column) => columnCompletionFor(tableIdx, column, range))
-    ];
+            documentation: `Access table ${tableIdx} using ${format}.`,
+            insertText: text
+        };
+    }),
+    ...tableInfo.columnNames.flatMap((column) => columnCompletionFor(tableIdx, tableInfo.variableName, column))
+];
+
+const objectCompletionFor = (objectIdx: number, objectInfo: InputObjectInfo) => {
+    const documentation = `Access the object at index ${objectIdx}.
+
+Current type : \`${objectInfo.objectType}\`  
+Current value : \` ${objectInfo.objectRepr} \`
+    `;
+    return {
+        label: objectInfo.variableName,
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        documentation: { value: documentation }, // NB: Implement IMarkdownString
+        insertText: objectInfo.variableName
+    };
 };
 
 export const registerMonacoInputColumnCompletions = (inputPortInfos: InputPortInfo[]) => {
-    // TODO make autocompletion faster by pre-computing the options and only adding the range to them
+    // Pre-compute the completions but without a range
+    type CompletionItemWithoutRange = Omit<monaco.languages.CompletionItem, 'range'>;
+    let tableIdx = 0;
+    let objectIdx = 0;
+    const completions = inputPortInfos.flatMap((portInfo): CompletionItemWithoutRange[] => {
+        if (portInfo.type === 'table') {
+            const tableInfo = portInfo as InputTableInfo;
+            return tableCompletionFor(tableIdx++, tableInfo);
+        } else {
+            const objectInfo = portInfo as InputObjectInfo;
+            return [objectCompletionFor(objectIdx++, objectInfo)];
+        }
+    });
+
+    // Register the completion provider using the computed completions
     monaco.languages.registerCompletionItemProvider('python', {
         provideCompletionItems(model, position) {
             const word = model.getWordUntilPosition(position);
@@ -55,18 +73,7 @@ export const registerMonacoInputColumnCompletions = (inputPortInfos: InputPortIn
                 startColumn: word.startColumn,
                 endColumn: word.endColumn
             };
-
-            return {
-                suggestions: inputPortInfos.flatMap((portInfo, tableIdx) => {
-                    if (portInfo.type === 'table') {
-                        const tableInfo = portInfo as InputTableInfo;
-                        return tableCompletionFor(tableIdx, tableInfo.columnNames, range);
-                    } else {
-                        // TODO add autocompletion for object ports
-                        return [];
-                    }
-                })
-            };
+            return { suggestions: completions.map((c) => ({ range, ...c })) };
         }
     });
 };
