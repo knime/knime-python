cbbb9f3bd5479e96ba33cd8bd2c3b6e7b9719c23
Adrian Nembach <adrian.nembach@knime.com>
2022-10-20 10:58:39 +0200

    Fix black formatting test on Windows

--- test_black_formatting.py
+++ test_black_formatting.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import sys
 import unittest
+import platform
 
 
 class BlackFormattingTest(unittest.TestCase):
@@ -12,8 +13,9 @@ class BlackFormattingTest(unittest.TestCase):
 
     def test_black_formatting(self):
         root_dir = os.path.normpath(os.path.join(__file__, ".."))
-        black_path = os.path.join(sys.prefix, "bin", "black")
-        res = subprocess.run([black_path, root_dir, "--check"], capture_output=True)
+        res = subprocess.run(
+            [self.get_black_path(), root_dir, "--check"], capture_output=True
+        )
         if res.returncode != 0:
             print(res.stdout.decode())
             print(res.stderr.decode(), file=sys.stderr)
@@ -24,3 +26,7 @@ class BlackFormattingTest(unittest.TestCase):
                     + res.stderr.decode()
                 )
             )
+
+    def get_black_path(self):
+        folder_name = "Scripts" if platform.system() == "Windows" else "bin"
+        return os.path.join(sys.prefix, folder_name, "black")
