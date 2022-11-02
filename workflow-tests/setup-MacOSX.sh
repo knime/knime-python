6e5d5ada1e80cdd14c770eb4eeaf95752b96de11
Carsten Haubold <carsten.haubold@knime.com>
2022-11-02 09:29:29 +0100

    Fix mac Python environment selection

--- workflow-tests/setup-MacOSX.sh
+++ workflow-tests/setup-MacOSX.sh
@@ -5,7 +5,7 @@ if [[ -n $KNIME_WORKFLOWTEST_PYTHON_VERSION ]]; then
 	echo "Setting python version to: ${KNIME_WORKFLOWTEST_PYTHON_VERSION}"
 
 	preferencesPath="${WORKSPACE}/workflow-tests/preferences-MacOSX.epf"
-	envPath="/Users/jenkins/miniconda3/envs/macos_py${KNIME_WORKFLOWTEST_PYTHON_VERSION}"
+	envPath="/Users/jenkins/miniconda3/envs/knime_py${KNIME_WORKFLOWTEST_PYTHON_VERSION}"
 	py3EnvPrefKey='python3CondaEnvironmentDirectoryPath='
 
 	sedi "s|${py3EnvPrefKey}.*|${py3EnvPrefKey}${envPath}|g" "${preferencesPath}"
