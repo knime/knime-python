43cde3dcca709aac9e18340bbf1fbdd871689041
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2022-12-06 15:45:31 +0100

    AP-19726: Improve plotly selection handling
    - Apply the initial selection once it is available
    - Use a Set for the selected points instead of a list
    - Get the current selection on "plotly_selected" events from the plot
      data instead of the event data. The event data does not contain all
      selected points if the plot consists of multiple subplots.
    - Deselect all subplots with an undefined selection on a selection
      event.
    
    AP-19726 (Plotly plots ignore initial selection)

--- org.knime.python3.views/src/main/python/knime/api/plotly-post-script.js
+++ org.knime.python3.views/src/main/python/knime/api/plotly-post-script.js
@@ -1,68 +1,106 @@
 const iFrameKnimeService = new KnimeUIExtensionService.IFrameKnimeService();
-const selectionService = new KnimeUIExtensionService.SelectionService(iFrameKnimeService);
+const selectionService = new KnimeUIExtensionService.SelectionService(
+  iFrameKnimeService
+);
 
-const d = document.getElementById('{plot_id}');
-let selected = []
+const plotlyPlot = document.getElementById("{plot_id}");
+let selected = new Set();
 
-
-/*
- * TODO get the initial selection from selectionService.initialSelection
- */
-
-/*
- * TODO `selected` should probably be a Set that is updated correctly
- * if selections in other views are added.
- */
+///////////////////////////////////////
+///////////////////////////////////////
+// TODO WRITE DOWN THE LIMITATIONS SOMEWHERE
+///////////////////////////////////////
+///////////////////////////////////////
 
 /*
- * TODO for facelets the selected data is only the data of the current
- * facelet. However, the selection in the other facelets does not change.
- * Therefore, calling replace is wong in this case.
+ * TODO BUG
+ * 1. Select some points in a plotly plot
+ * 2. Select some points in another plot
+ *    -> The plotly selection updates
+ * 3. Do a "shift" selection on the plotly plot
+ *    -> The selection from step 1 becomes visible again and the
+ *       selection from step 2 is deleted
  */
 
-d.on('plotly_selected', function (data) {
-    if (data === undefined) {
-        // Deselection is handled by on plotly_deselect
-        return;
-    }
+/////////////////////////////////////////////////////////////////////
+// KNIME Selection -> Plotly Selection
+/////////////////////////////////////////////////////////////////////
 
-    selected = data.points.map(p => p.customdata[0]);
-    selectionService.replace(selected);
-});
+function updateSelection(mode, selection) {
+  // Update the selected keys
+  if (mode == "REPLACE") {
+    selected = new Set(selection);
+  } else if (mode == "ADD") {
+    selection.forEach((s) => selected.add(s));
+  } else if (mode == "REMOVE") {
+    selection.forEach((s) => selected.delete(s));
+  }
 
-d.on('plotly_deselect', function () {
-    // Make sure we deselect everything: All facelets
-    Plotly.restyle(d, { selectedpoints: [null] });
-    selectionService.replace([]);
-});
-
-selectionService.addOnSelectionChangeCallback(event => {
-    // Update the selected keys
-    if (event.mode == "REPLACE") {
-        selected = event.selection;
-    } else if (event.mode == "ADD") {
-        selected.push(...event.selection);
-    } else if (event.mode == "REMOVE") {
-        selected = selected.filter(item => !event.selection.includes(item))
-    }
-
-    if (selected.length == 0) {
-        // Reset selection for all traces
-        Plotly.restyle(d, { selectedpoints: [null] });
-    } else {
-        // Loop over traces
-        for (let i = 0; i < d.data.length; i++) {
-
-            // Find the indices of the selected points for this trace
-            let indices = []
-            for (let j = 0; j < d.data[i].customdata.length; j++) {
-                if (selected.includes(d.data[i].customdata[j][0])) {
-                    indices.push(j);
-                }
-            }
-
-            // Update this trace
-            Plotly.restyle(d, { selectedpoints: [indices] }, [i]);
+  // Restyle the plot with the current selection
+  if (selected.size == 0) {
+    // Reset selection for all traces
+    Plotly.restyle(plotlyPlot, { selectedpoints: [null] });
+  } else {
+    // Loop over traces
+    for (let i = 0; i < plotlyPlot.data.length; i++) {
+      // Find the indices of the selected points for this trace
+      let indices = [];
+      for (let j = 0; j < plotlyPlot.data[i].customdata.length; j++) {
+        if (selected.has(plotlyPlot.data[i].customdata[j][0])) {
+          indices.push(j);
         }
+      }
+
+      // Update this trace
+      Plotly.restyle(plotlyPlot, { selectedpoints: [indices] }, [i]);
     }
+  }
+}
+
+iFrameKnimeService.waitForInitialization().then(() => {
+  // Register the selection listener
+  selectionService.onInit(
+    (event) => updateSelection(event.mode, event.selection),
+    true,
+    true
+  );
+
+  // NOTE: selectionService.initialSelection does not work because the implementation
+  // only returns an inital selection if initalData is not undefined
+
+  // Get the inital selection and apply it
+  const initialSelection =
+    selectionService.knimeService.extensionConfig.initialSelection;
+  updateSelection("REPLACE", initialSelection);
 });
+
+/////////////////////////////////////////////////////////////////////
+// Plotly Selection -> KNIME Selection
+/////////////////////////////////////////////////////////////////////
+
+plotlyPlot.on("plotly_selected", function () {
+  // Deselect all points on traces without any selection
+  plotlyPlot.data.forEach((d, idx) => {
+    if (d.selectedpoints === undefined) {
+      Plotly.restyle(plotlyPlot, { selectedpoints: [[]] }, [idx]);
+    }
+  });
+
+  // Get the customdata for the selected points of every trace
+  const selectedRows = plotlyPlot.data.flatMap((d) => {
+    return d.selectedpoints.map((i) => d.customdata[i][0]);
+  });
+
+  // Send the selection to the SelectionService
+  selectionService.replace(selectedRows);
+
+  // Remember the selection to be able to add points to it
+  selected = new Set(selectedRows);
+});
+
+plotlyPlot.on("plotly_deselect", function () {
+  // Make sure we deselect everything: All facelets
+  Plotly.restyle(plotlyPlot, { selectedpoints: [null] });
+  selectionService.replace([]);
+});
+
