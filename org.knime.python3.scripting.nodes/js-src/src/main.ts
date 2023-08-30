3e8b24328aaafc7a7d93ba3673dc58b604dcbd63
Carsten Haubold <carsten.haubold@knime.com>
2023-08-30 16:44:08 +0200

    Make sure that the scripting service is initialized first

--- org.knime.python3.scripting.nodes/js-src/src/main.ts
+++ org.knime.python3.scripting.nodes/js-src/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import "./python-scripting-service"; // to make sure that the scripting-service is initialized
 
 const app = createApp(App);
 
