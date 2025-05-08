8b2b3597a7fe528558623d49320527eefe71777f
Adrian Nembach <adrian.nembach@knime.com>
2025-05-08 11:30:33 +0200

    Adapt to ports in Arrow representation

--- org.knime.python3.arrow.types/src/main/python/knime/types/tool.py
+++ org.knime.python3.arrow.types/src/main/python/knime/types/tool.py
@@ -1,5 +1,21 @@
 from dataclasses import dataclass
+from typing import List, Optional
 import knime.api.types as kt
+
+
+class ToolPort:
+    name: str
+    description: str
+    type: str
+    spec: Optional[str]
+
+    def _to_arrow_dict(self):
+        return {
+            "0": self.name,
+            "1": self.description,
+            "2": self.type,
+            "3": self.spec,
+        }
 
 
 @dataclass
@@ -12,6 +28,8 @@ class WorkflowTool:
     description: str
     parameter_schema: dict
     tool_bytes: bytes
+    input_ports: Optional[List[ToolPort]] = None
+    output_ports: Optional[List[ToolPort]] = None
 
     # TODO extend with data in- and outputs
     def execute(self, parameters: dict) -> str: ...
@@ -46,4 +64,6 @@ class WorkflowToolValueFactory(kt.PythonValueFactory):
             "1": value.description,
             "2": value.parameter_schema,
             "3": value.tool_bytes,
+            "4": [port._to_arrow_dict() for port in value.input_ports],
+            "5": [port._to_arrow_dict() for port in value.output_ports],
         }
