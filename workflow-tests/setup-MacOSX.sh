9dca3dd7743ab9536a2e8f55cc87b7f30bdeec88
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-06-30 14:14:25 +0200

    AP-24557: Configure test nodes to run with test env (not bundled) and change `python_test_environment` name to be self-descriptive and fail builds with Python 3.6 env.
    AP-24557 (Run KNIME Python workflow testing nodes with testing environment, not bundled environment)

--- workflow-tests/setup-MacOSX.sh
+++ workflow-tests/setup-MacOSX.sh
@@ -9,7 +9,10 @@ if [[ -n $KNIME_WORKFLOWTEST_PYTHON_ENVIRONMENT ]]; then
 		cp "${WORKSPACE}/workflow-tests/preferences-bundled-env.epf" "${WORKSPACE}/workflow-tests/preferences-MacOSX.epf"
 		cat "${WORKSPACE}/workflow-tests/preferences-MacOSX.epf"
 	else
-		envPath="${WORKSPACE}/python_test_environment"
+		# remove extension substring from name
+		envName=${KNIME_WORKFLOWTEST_PYTHON_ENVIRONMENT%".yml"}
+
+		envPath="${WORKSPACE}/${envName}"
 		echo "Creating Conda environment for: ${KNIME_WORKFLOWTEST_PYTHON_ENVIRONMENT} at ${envPath}"
 
 		micromamba create \
@@ -20,5 +23,15 @@ if [[ -n $KNIME_WORKFLOWTEST_PYTHON_ENVIRONMENT ]]; then
 
 		sedi "s|<placeholder_for_env_path>|${envPath}|g" "${prefPath}"
 		cat "${prefPath}"
+
+		# Configure environment for Python-based nodes testing extension (py36 is not supported)
+		if [[ $KNIME_WORKFLOWTEST_PYTHON_ENVIRONMENT != env_py36* ]]; then
+			ext_config="${WORKSPACE}/workflow-tests/python-test-ext-config.yaml"
+			sedi "s|<placeholder_for_env_path>|${envPath}|g" "${ext_config}"
+			echo "-Dknime.python.extension.config=${ext_config}" >> "${WORKSPACE}/workflow-tests/vmargs"
+    else
+      echo "Python 3.6 is not supported to run workflow-tests"
+      exit 1
+		fi
 	fi
 fi
