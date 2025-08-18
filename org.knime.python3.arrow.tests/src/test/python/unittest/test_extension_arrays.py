ed6e7b1f1bd8b2d84593d067799f0114c9cbf9c5
Andrii Serheichuk <aserheichuk@n-ix.com>
2025-08-18 15:33:57 +0300

    AP-21518: Refactor test in order not to test string representation

--- org.knime.python3.arrow.tests/src/test/python/unittest/test_extension_arrays.py
+++ org.knime.python3.arrow.tests/src/test/python/unittest/test_extension_arrays.py
@@ -54,7 +54,6 @@ import string
 import numpy as np
 import pyarrow as pa
 
-import knime._arrow._backend as ka
 import knime._arrow._dictencoding as kasde
 import knime.api.types as kt
 import knime.api.schema as ks
@@ -505,18 +504,16 @@ class StructDictArrayTest(AbstractArrayTest, unittest.TestCase):
         lru = kasde.LRUCacheDict(cache_len=n)
         for i in range(n + 1):
             lru[str(i)] = i
-        correct = "LRUCacheDict([('1', 1), ('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7', 7), ('8', 8)])"
-        self.assertEqual(str(lru), correct)
+        for i in range(1, n):
+            self.assertEqual(lru[str(i)], i)
 
         # test lru strategy
-        l = [0, 1, 2, 3, 4]
+        lst = [0, 1, 2, 3, 4]
         lru = kasde.LRUCacheDict(cache_len=n)
-        for i in l:
+        for i in lst:
             lru[str(i)] = i
-        self.assertEqual(lru[str(0)], 0)
-        self.assertEqual(
-            str(lru), "LRUCacheDict([('1', 1), ('2', 2), ('3', 3), ('4', 4), ('0', 0)])"
-        )
+        for i in lst:
+            self.assertEqual(lru[str(i)], i)
 
 
 class MyTime:
